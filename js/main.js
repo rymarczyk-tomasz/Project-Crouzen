7863ef67a06f8c5a57a239c2ab05d371376bbb8a
Tomasz Rymarczyk <tomako0@gmail.com>
2023-02-14 22:11:15 +0100

    add gitignore, and few small changes

--- js/main.js
+++ js/main.js
@@ -2,12 +2,22 @@ const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
 const navBtnBars = document.querySelector('.burger-btn__bars')
+const bio = document.getElementById('bio')
 
 const handleNav = () => {
 	nav.classList.toggle('nav--disable')
 
 	handleNavItemsAnimation()
 }
+
+window.addEventListener('scroll', function(e) {
+	const position = window.pageYOffset;
+	if (position >= 300) {
+		bio.classList.add('active')
+	}
+	
+	console.log(position);
+});
 
 const handleNavItemsAnimation = () => {
 	let delayTime = 0
