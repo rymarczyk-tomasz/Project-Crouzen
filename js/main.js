0485bacea748d2c822312ed279afab34eaaff146
Tomasz <tomako0@gmail.com>
2023-02-15 11:43:48 +0100

    few small changes

--- js/main.js
+++ js/main.js
@@ -2,22 +2,54 @@ const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
 const navBtnBars = document.querySelector('.burger-btn__bars')
-const bio = document.getElementById('bio')
+
+
+function isScrollingDown() {
+	return window.scrollY > previousScrollY;
+  }
+
+  function getVisibleSections() {
+	const sections = document.querySelectorAll('section');
+	const visibleSections = [];
+  
+	for (let i = 0; i < sections.length; i++) {
+	  const section = sections[i];
+	  const rect = section.getBoundingClientRect();
+  
+	  if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+		visibleSections.push(section);
+	  }
+	}
+  
+	return visibleSections;
+  }
+
+  let previousScrollY = 0;
+
+window.addEventListener('scroll', function() {
+  const visibleSections = getVisibleSections();
+  const currentSection = visibleSections[visibleSections.length - 1];
+
+  if (isScrollingDown() && currentSection) {
+    // Użytkownik przewija w dół i istnieje następna sekcja
+	currentSection.classList.add('active')
+    // const nextSection = currentSection.nextElementSibling;
+    // Wykonaj akcję dla następnej sekcji
+  }
+//    else if (!isScrollingDown() && currentSection.previousElementSibling) {
+//     // Użytkownik przewija w górę i istnieje poprzednia sekcja
+//     const previousSection = currentSection.previousElementSibling;
+//     // Wykonaj akcję dla poprzedniej sekcji
+//   }
+
+  previousScrollY = window.scrollY;
+});
 
 const handleNav = () => {
-	nav.classList.toggle('nav--disable')
+	nav.classList.toggle('nav--active')
 
 	handleNavItemsAnimation()
 }
-
-window.addEventListener('scroll', function(e) {
-	const position = window.pageYOffset;
-	if (position >= 300) {
-		bio.classList.add('active')
-	}
-	
-	console.log(position);
-});
 
 const handleNavItemsAnimation = () => {
 	let delayTime = 0
