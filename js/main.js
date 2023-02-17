eefcb7e09926fbc51dc764d1a1fba064d748809d
Tomasz <tomako0@gmail.com>
2023-02-17 12:31:42 +0100

    change navigation, and burger button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const nav = document.querySelector('.nav')
-const navBtn = document.querySelector('.burger-btn')
-const allNavItems = document.querySelectorAll('.nav__item')
-const navBtnBars = document.querySelector('.burger-btn__bars')
+// const nav = document.querySelector('.nav')
+// const navBtn = document.querySelector('.burger-btn')
+// const allNavItems = document.querySelectorAll('.nav__item')
+// const navBtnBars = document.querySelector('.burger-btn__bars')
 
 
 function isScrollingDown() {
@@ -39,21 +39,47 @@ window.addEventListener('scroll', function() {
   previousScrollY = window.scrollY;
 });
 
-const handleNav = () => {
-	nav.classList.toggle('nav--active')
+// const handleNav = () => {
+// 	nav.classList.toggle('nav--active')
 
-	handleNavItemsAnimation()
-}
+// 	handleNavItemsAnimation()
+// }
 
-const handleNavItemsAnimation = () => {
-	let delayTime = 0
+// const handleNavItemsAnimation = () => {
+// 	let delayTime = 0
 
-	allNavItems.forEach(item => {
-		item.classList.toggle('nav-items-animation')
-		item.style.animationDelay = '.' + delayTime + 's'
-		delayTime++
-	})
-}
+// 	allNavItems.forEach(item => {
+// 		item.classList.toggle('nav-items-animation')
+// 		item.style.animationDelay = '.' + delayTime + 's'
+// 		delayTime++
+// 	})
+// }
 
-navBtn.addEventListener('click', handleNav)
-allNavItems.forEach(btn => btn.addEventListener('click', handleNav))
+// navBtn.addEventListener('click', handleNav)
+// allNavItems.forEach(btn => btn.addEventListener('click', handleNav))
+$(document).ready(function () {
+	// Add smooth scrolling to all links
+	$("a").on("click", function (event) {
+	  // Make sure this.hash has a value before overriding default behavior
+	  if (this.hash !== "") {
+		// Prevent default anchor click behavior
+		event.preventDefault();
+
+		// Store hash
+		const hash = this.hash;
+
+		// Using jQuery's animate() method to add smooth page scroll
+		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+		$("html, body").animate(
+		  {
+			scrollTop: $(hash).offset().top,
+		  },
+		  800,
+		  function () {
+			// Add hash (#) to URL when done scrolling (default click behavior)
+			window.location.hash = hash;
+		  }
+		);
+	  } // End if
+	});
+  });
